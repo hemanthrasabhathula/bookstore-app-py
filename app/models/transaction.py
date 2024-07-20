c90d054908b3479c3f6a31d6466eb1b579bd6df7
Hemanth Rasabhathula <hemanthrasabhathula@gmail.com>
2024-07-20 17:40:24 -0500

    Refactor get_all_user_transactions to sort by borrowedDate in descending order

--- app/models/transaction.py
+++ app/models/transaction.py
@@ -13,7 +13,7 @@ class Transaction:
 
     @staticmethod
     def get_all_user_transactions(user_id):
-        return list(Transaction.collection.find({"userId": ObjectId(user_id)}))
+        return list(Transaction.collection.find({"userId": ObjectId(user_id)}).sort('borrowedDate', DESCENDING))
 
     @staticmethod
     def get_copy_transactions(copy_id):
